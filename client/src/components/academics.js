fe05d505fc0dcfbe51986b86fb671856f5552a69
syedhasnain <syedhasnain9163@gmail.com>
2020-12-14 19:02:37 +0530

    commit 2

--- client/src/components/academics.js
+++ client/src/components/academics.js
@@ -1,9 +1,11 @@
 import '../css/academics.css';
+import Header from './header';
 
 export default function Academics(props)
 {
 	return <div>
-	
+	<h2 style={{backgroundColor:"lightgreen",fontSize:"25pt",fontStyle:'bold',color:"purple",padding:"10px",marginTop:"0%"}}>Academics</h2>
+			 <Header />
 			<button class='menu' data-toggle="collapse" data-target="#uni">University</button>
 
 			<div id="uni" class="collapse">
